3827c7a2e0781e04707a1b0701264e9365254ac3
MahmudNahid17 <149654192+MahmudNahid17@users.noreply.github.com>
2024-12-18 15:49:06 +0600

    Update A. I Wanna Be the Guy.cpp

--- A. I Wanna Be the Guy.cpp
+++ A. I Wanna Be the Guy.cpp
@@ -6,11 +6,11 @@ int main(){
     cin>>n;
     int p;
     cin>>p;
-    set<int>level;
+    set<int>level;// here set is a function for store distinct numbers.
     vector<int>arr(p);
     for(int i=0;i<p;i++){
         cin>>arr[i];
-        level.insert(arr[i]);
+        level.insert(arr[i]); // p array er evry single elemt input niye set function a insert kore dici.
 
     }
     int q;
@@ -18,9 +18,9 @@ int main(){
     vector<int>ar(q);
     for(int j=0;j<q;j++){
         cin>>ar[j];
-        level.insert(ar[j]);
+        level.insert(ar[j]);// q array er every single elemnt k input nye set function a insert kore dici.
     }
-    if(level.size()==n){
+    if(level.size()==n){ // set function a p,q er elment gula distinct vabe store hwar por seta k n er sathe comparison koreci
         cout<<"I become the guy."<<endl;
     }
     else{
