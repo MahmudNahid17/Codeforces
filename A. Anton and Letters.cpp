8ac658fdc562f40738c6609cf2a3dc4f6024d112
MahmudNahid17 <149654192+MahmudNahid17@users.noreply.github.com>
2024-12-19 15:25:38 +0600

    Update A. Anton and Letters.cpp

--- A. Anton and Letters.cpp
+++ A. Anton and Letters.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main(){
     set<char>letter;
     string s;
-    getline(cin,s);
+    getline(cin,s);// getline uses for taking input all element untill 
+                    // a new line is encountered
 
     for(char ch: s){
-        if(isalpha(ch)){
+        if(isalpha(ch)){ //isalpha reads only the letter type charecter
+                        // no special digt ,words,symbols will not include in isalpha
             letter.insert(ch);
         }
     }
