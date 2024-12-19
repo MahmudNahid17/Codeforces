507e6e75edd8955e569c5c0680a6509440e3881b
MahmudNahid17 <149654192+MahmudNahid17@users.noreply.github.com>
2024-12-19 23:53:33 +0600

    Update A. Line Breaks.cpp

--- A. Line Breaks.cpp
+++ A. Line Breaks.cpp
@@ -6,8 +6,8 @@ int main(){
     cin>>t;
 
     while(t--){
-         int n,m;
-         cin>>n>>m;
+         int n,m; // n holo word er sonkha.
+         cin>>n>>m; // m holo string er maximum length jeta m er moddhe store kora jabe.
 
          vector<int>v;
 
@@ -15,13 +15,13 @@ int main(){
              string s;
              cin>>s;
              int x=s.size();
-             v.push_back(x);
+             v.push_back(x); // string er size ta store kora hoyece v er moddhe .
 
          }
 
          int cnt=0;
          for(int i=0;i<v.size();i++){
-             m=m-v[i];
+             m=m-v[i];  // m means strip er sei maximum length jeta m er moddhe exactly fit hobe 
              if(m<0)
                 break;
                 cnt++;
